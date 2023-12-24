3a5968a0b81149eda0a5d90bec1c2fb7d089153d
gabigol_96@icloud.com <gabigol_96@icloud.com>
2023-12-24 15:59:34 +0000

    implemented theme change

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/ui/theme/theme-provider";
 import "./global.css";
 export default function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | Pizza Shop" />
-      <Toaster richColors />
-
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | Pizza Shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
