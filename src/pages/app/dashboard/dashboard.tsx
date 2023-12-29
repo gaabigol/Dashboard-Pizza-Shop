37350e727f59b7b138d6680c9091e1ccbd3a4808
gabigol_96@icloud.com <gabigol_96@icloud.com>
2023-12-29 20:38:19 +0000

    the entire visual part of the application has been completed

---
+++ src/pages/app/dashboard/dashboard.tsx
@@ -0,0 +1,30 @@
+import { Helmet } from "react-helmet-async";
+import { MonthRevenueCard } from "./month-revenue-card";
+import { MonthOrdersAmountCard } from "./month-revenue-amount";
+import { DayOrdersAmountCard } from "./day-orders-amount-card";
+import { MonthCanceledOrdersAmountCard } from "./month-canceled-orders";
+import RevenueChart from "./revenue-chart";
+import { PopularProductsChart } from "./popular-products-chart";
+
+export default function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight sm:text-2xl">
+          Dashboard
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          <MonthRevenueCard />
+          <MonthOrdersAmountCard />
+          <DayOrdersAmountCard />
+          <MonthCanceledOrdersAmountCard />
+        </div>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-2  lg:grid-cols-9 xl:grid-cols-9  gap-4">
+        <RevenueChart />
+        <PopularProductsChart />
+      </div>
+    </>
+  );
+}
